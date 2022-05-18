4ffb5e7731c1b9a225a661443c51273254aef6f0
Aleksandr Shalagin <ssanchesko@gmail.com>
2022-05-18 13:29:09 +0500

    Add test with CSS and webdriver manager

--- src/test/java/TestWeb.java
+++ src/test/java/TestWeb.java
@@ -1,3 +1,39 @@
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//public class TestWeb {
+//
+//    private WebDriver driver;
+//
+//    @BeforeAll
+//    public static void setUpAll() {
+//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+//    }
+//
+//    @BeforeEach
+//    public void setUp() {
+//        driver = new ChromeDriver();
+//    }
+//
+//    @AfterEach
+//    public void tearDown() {
+//        driver.quit();
+//        driver = null;
+//    }
+//
+//    @Test
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,30 +42,32 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestWeb {
+class ChromeTest {
 
-    private WebDriver driver;
+    WebDriver driver;
 
     @BeforeAll
-    public static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
-    public void setUp() {
+    void setupTest() {
         driver = new ChromeDriver();
     }
 
     @AfterEach
-    public void tearDown() {
-        driver.quit();
-        driver = null;
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
@@ -43,6 +81,19 @@ public class TestWeb {
         driver.findElement(By.className("checkbox")).click();
         driver.findElement(By.tagName("button")).click();
         String actual = driver.findElement(By.className("paragraph")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldTestFormCss() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79028383000");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         assertEquals(expected, actual);
 
