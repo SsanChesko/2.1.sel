15b1036287ca9b34db6f56a20f4cbc020d59de97
Aleksandr Shalagin <ssanchesko@gmail.com>
2022-05-19 15:58:53 +0500

    Add (options) to ChromeDriver, delete commended code, change tests

--- src/test/java/TestWeb.java
+++ src/test/java/TestWeb.java
@@ -1,43 +1,4 @@
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//public class TestWeb {
-//
-//    private WebDriver driver;
-//
-//    @BeforeAll
-//    public static void setUpAll() {
-//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
-//    }
-//
-//    @BeforeEach
-//    public void setUp() {
-//        driver = new ChromeDriver();
-//    }
-//
-//    @AfterEach
-//    public void tearDown() {
-//        driver.quit();
-//        driver = null;
-//    }
-//
-//    @Test
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -65,7 +26,7 @@ class ChromeTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -76,6 +37,7 @@ class ChromeTest {
     }
 
     @Test
+    @Disabled
     void shouldTestForm() {
         driver.get("http://localhost:9999/");
         List<WebElement> textFields = driver.findElements(By.className("input__control"));
@@ -107,7 +69,7 @@ class ChromeTest {
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79028383000");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector("button")).click();
-        String actual = driver.findElement(By.cssSelector(".input__sub")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         assertEquals(expected, actual);
     }
@@ -119,7 +81,7 @@ class ChromeTest {
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79028383000");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector("button")).click();
-        String actual = driver.findElement(By.cssSelector(".input__sub")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         assertEquals(expected, actual);
     }
@@ -131,7 +93,7 @@ class ChromeTest {
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79028383000");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector("button")).click();
-        String actual = driver.findElement(By.cssSelector(".input__sub")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
         String expected = "Поле обязательно для заполнения";
         assertEquals(expected, actual);
     }
@@ -143,8 +105,7 @@ class ChromeTest {
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+790283830000");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector("button")).click();
-        List<WebElement> textFields = driver.findElements(By.cssSelector(".input__sub"));
-        String actual = textFields.get(1).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
         assertEquals(expected, actual);
     }
@@ -166,7 +127,6 @@ class ChromeTest {
         driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("");
-//    driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector("button")).click();
         String actual = driver.findElement(By.cssSelector("[data-test-id='agreement'].checkbox .checkbox__text")).getText();
         String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
